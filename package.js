37401cce64bf1b7fa21e62bd7a014c43ecabc950
Sebastián <sebastiaan.diaz.prado@gmail.com>
2026-05-11 21:11:51 -0600

    Initialize package.json with project details

--- package.js
+++ package.js
@@ -1,1 +1,13 @@
-
+{
+  "name": "tesis-extractor-api",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "express": "^4.19.2",
+    "pdfjs-dist": "3.11.174"
+  }
+}
